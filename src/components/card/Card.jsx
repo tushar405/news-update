c7e994253162b80d31f443ce392a1d363df9dcba
tushar---project <tusharkum0@gmail.com>
2026-01-06 22:38:33 +0530

    Fix Card.jsx duplicate code

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,53 +3,28 @@ import './Card.css';
 
 const Card = ({ data }) => {
   console.log(data);
-  const readMore = (url) =>{
-    window.open(url);
-  }
+
+  const readMore = (url) => {
+    window.open(url, '_blank'); // safer to open in new tab
+  };
+
   return (
     <div className='card-container'>
       {data
-      .filter((currData) => currData.urlToImage)
-      .map((currData, index) => (
-
-
-        <div className="card" key={index}>
-          <img src={currData.urlToImage} alt="" />
-          <div className="content">
-            <a onClick={()=> readMore(currData.url)}className='title'>{currData.title ? currData.title.slice(0, 70) + "..." : ""}</a>
-            <p className='description'>{currData.description ? currData.description.slice(0, 95) + "..." : ""}</p>
-            <button onClick={()=> readMore(currData.url)}>Read More</button>
+        .filter((currData) => currData.urlToImage)
+        .map((currData, index) => (
+          <div className="card" key={index}>
+            <img src={currData.urlToImage} alt={currData.title || "news image"} />
+            <div className="content">
+              <a onClick={() => readMore(currData.url)} className='title'>
+                {currData.title ? currData.title.slice(0, 70) + "..." : ""}
+              </a>
+              <p className='description'>
+                {currData.description ? currData.description.slice(0, 95) + "..." : ""}
+              </p>
+              <button onClick={() => readMore(currData.url)}>Read More</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Card;
-import React from 'react';
-import './Card.css';
-
-const Card = ({ data }) => {
-  console.log(data);
-  const readMore = (url) =>{
-    window.open(url);
-  }
-  return (
-    <div className='card-container'>
-      {data
-      .filter((currData) => currData.urlToImage)
-      .map((currData, index) => (
-
-
-        <div className="card" key={index}>
-          <img src={currData.urlToImage} alt="" />
-          <div className="content">
-            <a onClick={()=> readMore(currData.url)}className='title'>{currData.title ? currData.title.slice(0, 70) + "..." : ""}</a>
-            <p className='description'>{currData.description ? currData.description.slice(0, 95) + "..." : ""}</p>
-            <button onClick={()=> readMore(currData.url)}>Read More</button>
-          </div>
-        </div>
       ))}
     </div>
   );
